7019c04f06ebb96a08240ea55db30a06370fb623
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-08-27 22:56:49 +0330

    fix: logo Navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -9,9 +9,9 @@ import Logo from "../../../public/logo.png";
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex items-center justify-between sm:px-12 px-2 md:px-24 ">
+    <div className="flex items-center justify-between sm:px-12 px-2 md:px-1 ">
       <Link href="/" className="flex items-center justify-center h-20">
-        <Image src={Logo} alt="Logo" className="h-full" />
+        <Image src={Logo} alt="Logo" width={350} />
       </Link>
       <div className="flex items-center">
         <button className="bg-[#FF9916] text-white px-2 py-1 sm:px-4 rounded-b-md text-sm font-medium hover:text-[#FF9916] hover:bg-white hover:border-2 hover:border-[#FF9916] border-2 border-transparent transition duration-300 ease-in-out">
