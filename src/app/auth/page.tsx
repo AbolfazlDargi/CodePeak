840433c42ea2621786f4e14b3b5925f802a9a908
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-08-28 00:19:45 +0330

    feat: AuthModal Layout UI

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,22 +1,21 @@
-import Navbar from "@/components/Navbar/Navbar"
-import React from 'react'
-import Image from "next/image"
-import Hero from "../../../public/hero.png"
+import Navbar from "@/components/Navbar/Navbar";
+import React from "react";
+import Image from "next/image";
+import Hero from "../../../public/hero.png";
+import AuthModal from "@/components/Models/AuthModal";
 
-
-
-const Authpage:React.FC = () => {
-    return (
-        <div className='bg-gradient-to-b from-gray-600 to-black h-screen relative'>
-            <div className='max-w-7xl mx-auto'>
-            <Navbar/>
-            </div>
-            <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
-                <Image src={Hero} alt="Hero img"></Image>
-            </div>
+const Authpage: React.FC = () => {
+  return (
+    <div className="bg-gradient-to-b from-gray-600 to-black h-screen relative">
+      <div className="max-w-7xl mx-auto">
+        <Navbar />
+        <div className="flex items-center justify-center h-[calc(100vh-5rem)] pointer-events-none select-none">
+          <Image src={Hero} alt="Hero img"></Image>
         </div>
-    )
-}
+        <AuthModal/>
+      </div>
+    </div>
+  );
+};
 
-export default Authpage
-
+export default Authpage;
---
+++ src/components/Models/AuthModal.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { IoClose } from "react-icons/io5";
+import Login from "./Login"
+
+// type Props = {}
+
+const AuthModal: React.FC = () => {
+  return (
+    <>
+      <div className="absolute top-0 left-0 w-full flex items-center justify-center bg-black opacity-60"></div>
+      <div className="w-full sm:w-[450px] absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] flex justify-center items-center">
+        <div className="relative w-full h-full mx-auto flex items-center justify-center">
+          <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-[#FF9916] to-slate-900 mx-6">
+            <div className="fle justify-end p-2">
+              <button
+                type="button"
+                className="bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white">
+                <IoClose className="h-5 w-5" />
+              </button>
+            </div>
+            <Login/>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AuthModal;
