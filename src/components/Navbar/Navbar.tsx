68681848ff110c47d01bd3a7769c7c4a8a7242f2
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-08-27 17:11:55 +0330

    feat: Auth page Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type NavbarProps = {
+  name: string
+}
+
+const Navbar:React.FC<NavbarProps> = () => {
+  return (
+    <div className='flex items-center justify-between '>
+
+    </div>
+  )
+}
