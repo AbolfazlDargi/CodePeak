026fbd64276ce643366db3320c7f4c43f1354db8
Abolfazl Dargi <abolfazl.dargi.3@gmail.com>
2025-10-10 01:28:30 +0330

    feat: confetti Celebration

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,16 +1,20 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Split from "react-split";
 import ProblemDescription from "./problemDescription/problemDescription";
 import Playground from "./Playground/Playground";
 import { Problem } from "@/utils/types/problem";
+import Confetti from "react-confetti";
+import useWindowSize from "@/hooks/useWindowSize";
 
 type WorkspaceProps = {
   problem: Problem;
 };
 
 const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
+  const { width, height } = useWindowSize();
+  const [success, setSuccess] = useState(false);
   return (
     <Split
       className="flex flex-row h-screen relative"
@@ -26,14 +30,14 @@ const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
         gutterDiv.style.cursor = "col-resize";
 
         gutterDiv.addEventListener("mouseleave", () => {
-            gutterDiv.style.backgroundColor = "#9ca3af";
-            dot.style.backgroundColor = "#fbbf24";
-        })
+          gutterDiv.style.backgroundColor = "#9ca3af";
+          dot.style.backgroundColor = "#fbbf24";
+        });
 
         gutterDiv.addEventListener("mouseenter", () => {
-            gutterDiv.style.backgroundColor = "#374151";
-            dot.style.backgroundColor = "#9ca3af";
-        })
+          gutterDiv.style.backgroundColor = "#374151";
+          dot.style.backgroundColor = "#9ca3af";
+        });
 
         const dot = document.createElement("div");
         dot.className = "bg-gray-400 rounded-full absolute";
@@ -46,8 +50,16 @@ const Workspace: React.FC<WorkspaceProps> = ({ problem }) => {
         gutterDiv.appendChild(dot);
         return gutterDiv;
       }}>
-      <ProblemDescription problem={problem}/>
-      <Playground problem={problem}/>
+      <ProblemDescription problem={problem} />
+      <Playground problem={problem} />
+      {success && (
+        <Confetti
+          width={width}
+          height={height}
+          gravity={0.3}
+          tweenDuration={4000}
+        />
+      )}
     </Split>
   );
 };
